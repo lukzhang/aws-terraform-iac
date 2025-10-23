64157115fdf5e02f326f76cef93cbc543f610234
lukzhang <l_zhang18@hotmail.com>
2025-10-22 23:45:05 -0400

    update

--- iam.tf
+++ iam.tf
@@ -29,5 +29,5 @@ resource "aws_iam_role" "codepipeline_role" {
 
 resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
   role       = aws_iam_role.codepipeline_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipeline_FullAccess"
 }
